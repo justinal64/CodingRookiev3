d4b89b2cab02445f5eb4c39b5f193cf1e8581cdc
justin leggett <justin@codingrookie.com>
2019-02-07 21:04:28 -0600

    added nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,12 @@
 import React, { Component } from "react";
-import SimpleCard from "./components/Test";
-import { Spring } from "react-spring";
-import { Transition } from "react-spring";
-import logo from "./logo.svg";
+import ButtonAppBar from "./components/navigation/navigation";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <SimpleCard />
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-            <Spring from={{ opacity: 0 }} to={{ opacity: 1 }}>
-              {props => <div style={props}>hello</div>}
-            </Spring>
-          </a>
-        </header>
+        <ButtonAppBar />
       </div>
     );
   }
---
+++ src/components/navigation/navigation.tsx
@@ -0,0 +1,77 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { createStyles, withStyles, WithStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+
+const styles = createStyles({
+  root: {
+    flexGrow: 1
+  },
+  grow: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20
+  }
+});
+
+export interface Props extends WithStyles<typeof styles> {}
+
+function ButtonAppBar(props: Props) {
+  const { classes } = props;
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  function handleClick(event: any) {
+    setAnchorEl(event.currentTarget);
+  }
+
+  function handleClose() {
+    setAnchorEl(null);
+  }
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="Menu"
+          >
+            <MenuIcon>
+              <Menu
+                id="simple-menu"
+                anchorEl={anchorEl}
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={handleClose}>Profile</MenuItem>
+                <MenuItem onClick={handleClose}>My account</MenuItem>
+                <MenuItem onClick={handleClose}>Logout</MenuItem>
+              </Menu>
+            </MenuIcon>
+          </IconButton>
+          <Typography variant="h6" color="inherit" className={classes.grow} />
+          <Button color="inherit">Tutorials</Button>
+          <Button color="inherit">Blog</Button>
+          <Button color="inherit">Store</Button>
+          <Button color="inherit">Login</Button>
+          <Button color="inherit">Sign Up</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
+ButtonAppBar.propTypes = {
+  classes: PropTypes.object.isRequired
+} as any;
+
+export default withStyles(styles)(ButtonAppBar);
