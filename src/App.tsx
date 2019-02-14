7df66dbbe24f71c042b33de3a7a235b721d1d2eb
justin leggett <justin@codingrookie.com>
2019-02-13 22:16:38 -0600

    added redirects fiel for netlify and added switch so 404 is only shown when no other page is found

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import ButtonAppBar from "./components/navigation/navigation";
 import "./App.css";
 import { Header } from "./components/header/header";
 import { useMath } from "./components/customHooks/useMath/useMath";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import { Home } from "./pages/home";
 import { Api } from "./pages/api";
 import { Login } from "./pages/login";
@@ -24,15 +24,17 @@ export function App() {
         <h1>Add</h1>
         {tester}
         <div>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/api" component={Api} />
-          <Route exact path="/tutorials" component={Tutorials} />
-          <Route exact path="/store" component={Store} />
-          <Route exact path="/signup" component={Signup} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/blog" component={Blog} />
-          <Route exact path="/about" component={About} />
-          <Route component={NoMatch} />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/api" component={Api} />
+            <Route exact path="/tutorials" component={Tutorials} />
+            <Route exact path="/store" component={Store} />
+            <Route exact path="/signup" component={Signup} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/blog" component={Blog} />
+            <Route exact path="/about" component={About} />
+            <Route component={NoMatch} />
+          </Switch>
         </div>
       </div>
     </Router>
