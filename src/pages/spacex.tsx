dbd39b36bed6a9b010bcc9b57c84e75cde0d5c06
justin leggett <justin@codingrookie.com>
2019-02-26 22:02:42 -0600

    spacex page is rendering properly. I just need to work on the layout of the data I'm displaying

--- src/pages/spacex.tsx
+++ src/pages/spacex.tsx
@@ -3,20 +3,35 @@ import { useFetch } from "../components/customHooks/useFetch/useFetch";
 
 export const SpaceX = () => {
   const data = useFetch();
-  let dataFromApi = "";
 
   if (data) {
-    data.map((ele: any) => {
-      dataFromApi = `Hi my first name is ${
-        ele.first_name
-      } and my last name is ${ele.last_name} `;
-    });
+    // console.log(data);
+    // console.log(data["details"]);
+    // Object.keys(data).forEach((element: any) => {
+    //   console.log(data[element]);
+    // });
   }
+
+  let test = () => {
+    Object.keys(data || {}).map((element: any) => {
+      console.log("element: ", data[element]);
+      return <div>{data[element]}</div>;
+    });
+  };
 
   return (
     <div>
       <h2>SpaceX</h2>
-      {dataFromApi}
+      {Object.keys(data || {}).map((element: any) => {
+        console.log("element: ", data[element]);
+        return (
+          <div>
+            <a href={data[element]} key={data[element]}>
+              {data[element]}
+            </a>
+          </div>
+        );
+      })}
     </div>
   );
 };
