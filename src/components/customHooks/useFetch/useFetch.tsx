771fdae820ca1bffd749493c3beee8f2f5694aaf
justin leggett <justin@codingrookie.com>
2019-02-26 21:00:37 -0600

    displaying data correctly

--- src/components/customHooks/useFetch/useFetch.tsx
+++ src/components/customHooks/useFetch/useFetch.tsx
@@ -1,46 +1,45 @@
 import React, { useState, useEffect } from "react";
 
 export interface jsondata {
-  username: string;
-  first_name: string;
-  last_name: string;
-  kids: number;
-  spouse: string;
-  missions: {
-    name: string;
-    flight: number;
+  username?: string;
+  first_name?: string;
+  last_name?: string;
+  kids?: number;
+  spouse?: string;
+  missions?: {
+    name?: string;
+    flight?: number;
   };
-  details: string;
+  details?: string;
 }
 
-let jsonData: jsondata = {
-  username: "",
-  first_name: "",
-  last_name: "",
-  kids: 0,
-  spouse: "",
-  missions: {
-    name: "",
-    flight: 0
-  },
-  details: ""
-};
+let jsonData: jsondata[] = [
+  {
+    username: "",
+    first_name: "",
+    last_name: "",
+    kids: 0,
+    spouse: "",
+    missions: {
+      name: "",
+      flight: 0
+    },
+    details: ""
+  }
+];
 
-export function useFetch(url: string) {
-  const [data, setData] = useState(jsonData);
+export function useFetch() {
+  const [data, setData] = useState(null);
 
-  const setSpacexData = (newData: jsondata) => setData(newData);
+  const setSpacexData = (newData: any) => setData(newData);
 
   useEffect(() => {
-    fetchData("Test");
-  }, []);
-
-  let fetchData = (api: string) => {
     fetch("data.json") // runs command from the public folder.
       .then(res => res.json())
       .then(data => {
         setSpacexData(data);
       });
-  };
+  }, []);
+
   return data;
 }
--- src/pages/spacex.tsx
+++ src/pages/spacex.tsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useFetch } from "../components/customHooks/useFetch/useFetch";
 
 export const SpaceX = () => {
-  const data = useFetch("add");
-  console.log("data: ", data); // data is logging to the console. Now get it showing up on screen.
+  const data = useFetch();
+  let dataFromApi = "";
 
-  return <div>{/* {data}<h2>SpaceX</h2> */}</div>;
+  if (data) {
+    data.map((ele: any) => {
+      dataFromApi = `Hi my first name is ${
+        ele.first_name
+      } and my last name is ${ele.last_name} `;
+    });
+  }
+
+  return (
+    <div>
+      <h2>SpaceX</h2>
+      {dataFromApi}
+    </div>
+  );
 };
