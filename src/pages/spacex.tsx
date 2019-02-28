6e989f98e096c369a2d4f3029a01fd195d6630be
justin leggett <justin@codingrookie.com>
2019-02-27 21:51:45 -0600

    rendering to page

--- src/pages/spacex.tsx
+++ src/pages/spacex.tsx
@@ -1,34 +1,20 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useFetch } from "../components/customHooks/useFetch/useFetch";
 
 export const SpaceX = () => {
   const data = useFetch();
 
-  if (data) {
-    // console.log(data);
-    // console.log(data["details"]);
-    // Object.keys(data).forEach((element: any) => {
-    //   console.log(data[element]);
-    // });
-  }
-
-  let test = () => {
-    Object.keys(data || {}).map((element: any) => {
-      console.log("element: ", data[element]);
-      return <div>{data[element]}</div>;
-    });
+  let tester = () => {
+    return <div>Hi!!!!</div>;
   };
 
   return (
     <div>
       <h2>SpaceX</h2>
-      {Object.keys(data || {}).map((element: any) => {
-        console.log("element: ", data[element]);
+      {Object.keys(data || {}).map((element: any, index) => {
         return (
-          <div>
-            <a href={data[element]} key={data[element]}>
-              {data[element]}
-            </a>
+          <div key={index}>
+            <a href={data[element]}>{data[element]}</a>
           </div>
         );
       })}
