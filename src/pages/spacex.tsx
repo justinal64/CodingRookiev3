77e392516191f54303f727147ffd0531e91724af
justin leggett <justin@codingrookie.com>
2019-02-28 20:49:54 -0600

    Rendering properly

--- src/pages/spacex.tsx
+++ src/pages/spacex.tsx
@@ -3,21 +3,26 @@ import { useFetch } from "../components/customHooks/useFetch/useFetch";
 
 export const SpaceX = () => {
   const data = useFetch();
+  let array = [1, 2, 3, 4, 5];
 
-  let tester = () => {
-    return <div>Hi!!!!</div>;
+  let test = (arrayToMap: any) => {
+    let result: JSX.Element[] = [];
+    {
+      Object.keys(data || []).map((element: any, index) => {
+        result.push(
+          <div key={index}>
+            <a href={data[element]}>{data[element]}</a>
+          </div>
+        );
+      });
+    }
+    return result;
   };
 
   return (
     <div>
       <h2>SpaceX</h2>
-      {Object.keys(data || {}).map((element: any, index) => {
-        return (
-          <div key={index}>
-            <a href={data[element]}>{data[element]}</a>
-          </div>
-        );
-      })}
+      {test(data)}
     </div>
   );
 };
