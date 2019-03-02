e2afbd4474d9ad7b06302feeda6e64fc875e3142
justin leggett <justin@codingrookie.com>
2019-03-01 21:38:14 -0600

    add and subtract are working correctly

--- src/pages/calculator/calculator.tsx
+++ src/pages/calculator/calculator.tsx
@@ -1,17 +1,18 @@
 import React, { useState } from "react";
-import { useMath } from "../../components/customHooks/useMath/useMath";
 import { Button, TextField } from "@material-ui/core";
 import "../../styles/Calculator.css";
 
+let buggyInitialState: any = 0;
+
 export const Calculator = () => {
-  // const number = useMath([1, 2, 3, 4, 5], "add");
   const [input0, setInput0] = useState(0);
   const [input1, setInput1] = useState(0);
-  const [input2, setInput2] = useState("");
-  const [input3, setInput3] = useState("");
-  let Add = () => {
+  const [input2, setInput2] = useState(buggyInitialState);
+  const [input3, setInput3] = useState(buggyInitialState);
+  let Add = (num: any, num2: any) => {
+    // I have to use type :any to show the bug
     console.log("You clicked the add button");
-    console.log("input0 + input0 =  ", input0 + input1);
+    console.log("input0 + input0 =  ", num + num2);
   };
 
   let Subtract = () => {
@@ -28,36 +29,64 @@ export const Calculator = () => {
   };
 
   // function to check data
-  let verifyInput = () => {
-    // this function should test that the data is a valid number if not return 0
+  let verifyInput = (): Boolean => {
+    if (!isNaN(input0) && !isNaN(input1)) {
+      return true;
+    } else {
+      return false;
+    }
   };
 
   return (
     <div>
       <div>
         <h2>Calculator working correctly</h2>
-        <Button variant="contained" color="primary" onClick={Add}>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => {
+            if (verifyInput()) Add(input0, input1);
+          }}
+        >
           Add
         </Button>
-        <Button variant="contained" color="primary" onClick={Subtract}>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => {
+            if (verifyInput()) Subtract();
+          }}
+        >
           Subtract
         </Button>
-        <Button variant="contained" color="primary" onClick={Multiply}>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => {
+            if (verifyInput()) Multiply();
+          }}
+        >
           Multiply
         </Button>
-        <Button variant="contained" color="primary" onClick={Divide}>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => {
+            if (verifyInput()) Divide();
+          }}
+        >
           Divide
         </Button>
         <div>
           <input
             value={input0}
-            onChange={(e: any) => {
+            onChange={e => {
               setInput0(parseInt(e.target.value));
             }}
           />
           <input
             value={input1}
-            onChange={(e: any) => setInput1(parseInt(e.target.value))}
+            onChange={e => setInput1(parseInt(e.target.value))}
           />
         </div>
       </div>
@@ -65,7 +94,11 @@ export const Calculator = () => {
         <div>
           {/* Do not use the validateInput function with this section */}
           <h2>Calculator with bugs</h2>
-          <Button variant="contained" color="primary" onClick={Add}>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => Add(input2, input3)}
+          >
             Add
           </Button>
           <Button variant="contained" color="primary" onClick={Subtract}>
@@ -77,6 +110,15 @@ export const Calculator = () => {
           <Button variant="contained" color="primary" onClick={Divide}>
             Divide
           </Button>
+          <div>
+            <input
+              value={input2}
+              onChange={e => {
+                setInput2(e.target.value);
+              }}
+            />
+            <input value={input3} onChange={e => setInput3(e.target.value)} />
+          </div>
         </div>
       </div>
     </div>
